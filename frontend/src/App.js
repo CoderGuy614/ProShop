22e22e5f2705e83c8d530450a07ffb3cca8976c8
CoderGuy614 <jlutz.110@gmail.com>
2020-10-26 18:15:50 -0400

    Install Font End Dependencies, Create Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
   return (
     <>
+    <Header />
+<Container>
+<main className='py-3'>
      <h1>Welcome to the Shop</h1>
+    </main>
+</Container>
+
+    <Footer />
     </>
   );
 }
