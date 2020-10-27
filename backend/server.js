211c081178a2e62c3e3c4f054e889ab1bccb47c0
CoderGuy614 <jlutz.110@gmail.com>
2020-10-27 15:47:05 -0400

    Connect to MongoDB and Connfig DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 app.get("/api/products", (req, res) => {
   res.json(products);
@@ -18,5 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT} `)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
