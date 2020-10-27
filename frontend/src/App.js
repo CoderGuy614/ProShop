9542b3bf0a76ebaf4600c7d279d0fab530ec75f9
CoderGuy614 <jlutz.110@gmail.com>
2020-10-26 21:23:21 -0400

    Add Bootstrap Cards and Product Data to Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,17 @@ import { Container } from 'react-bootstrap'
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
     <Header />
-<Container>
-<main className='py-3'>
-     <h1>Welcome to the Shop</h1>
-    </main>
-</Container>
-
+      <Container>
+        <main className='py-3'>
+        <HomeScreen />
+        </main>
+      </Container>
     <Footer />
     </>
   );
