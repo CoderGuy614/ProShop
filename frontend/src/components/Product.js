bac55827165aea3bc865ac8a0cc1e77753fdba76
CoderGuy614 <jlutz.110@gmail.com>
2020-10-27 07:27:46 -0400

    Add Star Rating to Product

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "../components/Rating";
 
-const Product = ({product}) => {
-    return (
-        <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant='bottom' />
-            </a>
-            <Card.Body>
-            <a href={`/product/${product._id}`}>
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </a>
-            <Card.Text as='div'>
-                <div className='my-3'>
-                    {product.rating} from {product.numReviews} reviews
-                </div>
-            </Card.Text>
-            <Card.Text as='h3'>
-                <div className='my-3'>
-                    ${product.price}
-                </div>
-            </Card.Text>'
-            </Card.Body>
-            </Card>
-    )
-}
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="bottom" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">
+          <div className="my-3">${product.price}</div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+Rating.defaultProps = {
+  color: "#f8e825",
+};
 
-export default Product
+export default Product;
