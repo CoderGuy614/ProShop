2e5d925530e5f253e1df929917f3e418ab70d621
CoderGuy614 <jlutz.110@gmail.com>
2020-10-28 11:13:45 -0400

    Create basic product routes and custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-const app = express();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
-
 connectDB();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+const app = express();
+app.use("/api/products/", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
