0d8d532f1da6d583f58d7c6e514acd637858640d
CoderGuy614 <jlutz.110@gmail.com>
2020-11-01 11:57:53 -0500

    Generate Token and Send to Client on Login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 const router = express.Router();
 import { authUser } from "../controllers/userController.js";
 
-router.route("/login").post(authUser);
+router.post("/login", authUser);
 
 export default router;
