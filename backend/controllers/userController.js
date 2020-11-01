a5d8087c5706b04a37a6fcd683c67d9c2023d0ca
CoderGuy614 <jlutz.110@gmail.com>
2020-11-01 13:04:20 -0500

    Finish User Auth Backend Functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,4 +22,52 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@description Register a new user
+//@route POST /api/users/
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+//@description Get User Profile
+//@route GET /api/users/profile
+//@access Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("Invalid email or password");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
